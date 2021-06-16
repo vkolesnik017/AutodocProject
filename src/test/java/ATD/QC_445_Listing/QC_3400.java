be7a380878251d123eed30c718fed30d8f7a8ae1
vkolesnik017 <52042636+vkolesnik017@users.noreply.github.com>
2021-06-17 01:25:34 +0300

    QC-3400

--- src/test/java/ATD/QC_445_Listing/QC_3400.java
+++ src/test/java/ATD/QC_445_Listing/QC_3400.java
@@ -11,6 +11,7 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.sql.SQLException;
+import java.util.Arrays;
 import java.util.List;
 
 import static ATD.CommonMethods.openPage;
@@ -36,6 +37,7 @@ public class QC_3400 {
     @Description(value = "test checks Sorting on the current issue by default, provided that there is more than one generic product in the issue")
     public void testCheckSortingInTecDocListing(String route) {
         openPage(route);
+        List<String> newList = Arrays.asList("Hello");
         List<String> expectedGenerics = motoCategoryPage
                 .displayGenericFilterBlock()
                 .getTitleOfGenerics();
